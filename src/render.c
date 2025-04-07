f6802ac1e592bd48781c050929588a215cf0fa3f
SuperPuiu <SuperPuiu@proton.me>
2025-04-07 22:09:40 +0300

    render: go back to generalized buffers due to problems with right click menu

--- src/render.c
+++ src/render.c
@@ -10,21 +10,14 @@
 
 #define BUFFER_SIZE 512
 
-typedef struct {
-  mu_Rect Texture;
-  mu_Rect Source;
-  mu_Color Color;
-} TextureData;
-
 static uint32_t Background;
 
 static mu_Rect Clip;
+static mu_Rect TextureBuffer[BUFFER_SIZE];
+static mu_Rect SourceBuffer[BUFFER_SIZE];
+static mu_Color ColorBuffer[BUFFER_SIZE]; 
 
-TextureData TextData[BUFFER_SIZE];
-TextureData OtherData[BUFFER_SIZE];
-
-static unsigned int OtherIndex = 0;
-static unsigned int TextIndex = 0;
+static unsigned int BufferIndex = 0;
 
 static inline uint32_t ColorToNumber(mu_Color Color) {
   return ((uint32_t)Color.a << 24) | ((uint32_t)Color.r << 16) | ((uint32_t)Color.g << 8) | Color.b;
@@ -43,7 +36,7 @@ static inline uint8_t GetAtlasColor(mu_Rect *Texture, int X, int Y) {
   X += Texture->x;
   assert(Y < Texture->h);
   Y += Texture->y;
-
+  
   return atlas_texture[Y * ATLAS_WIDTH + X];
 }
 
@@ -52,19 +45,19 @@ static inline mu_Color BlendPixel(mu_Color Destination, mu_Color Source) {
   Destination.r = ((Source.r * Source.a) + (Destination.r * ia)) >> 8;
   Destination.g = ((Source.g * Source.a) + (Destination.g * ia)) >> 8;
   Destination.b = ((Source.b * Source.a) + (Destination.b * ia)) >> 8;
-
   return Destination;
 }
 
 void r_init() {
   OpenWindow();
-  Background = ColorToNumber(mu_color(44, 44, 44, 255));
+  Background = ColorToNumber(mu_color(33, 33, 33, 255));
   Clip = mu_rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 void FlushBuffers() {
-  for (unsigned int i = 0; i < OtherIndex; i++) {
-    mu_Rect *Source = &OtherData[i].Source;
+  for (unsigned int i = 0; i < BufferIndex; i++) {
+    mu_Rect *Source = &SourceBuffer[i];
+    mu_Rect *Texture = &TextureBuffer[i];
 
     unsigned int Y = mu_max(Source->y, Clip.y);
     unsigned int X = mu_max(Source->x, Clip.x);
@@ -76,50 +69,34 @@ void FlushBuffers() {
         assert(InRectangle(*Source, CurrentX, CurrentY));
         assert(InRectangle(Clip, CurrentX, CurrentY));
         
-        PAP_PutPixel(CurrentX, CurrentY, ColorToNumber(OtherData[i].Color));
+        /* Text */
+        if (Source->w == Texture->w && Source->h == Texture->h) {
+          uint8_t TextureColor = GetAtlasColor(Texture, CurrentX - Source->x, CurrentY - Source->y);
+          uint32_t CurrentPixel = PAP_GetPixel(CurrentX, CurrentY);
+          PAP_PutPixel(CurrentX, CurrentY, CurrentPixel | (int)ColorToNumber(mu_color(TextureColor, TextureColor, TextureColor, 255)));
+        /* Other */
+        } else {
+          mu_Color NewColor = BlendPixel(NumberToColor(PAP_GetPixel(CurrentX, CurrentY)), ColorBuffer[i]);
+          uint32_t PixelColor = ColorToNumber(NewColor);
+          PAP_PutPixel(CurrentX, CurrentY, PixelColor);
+        }
+
       }
     }
   }
-
-  for (unsigned int i = 0; i < TextIndex; i++) {
-    mu_Rect *Source = &TextData[i].Source;
-    mu_Rect *Texture = &TextData[i].Texture;
-
-    unsigned int Y = mu_max(Source->y, Clip.y);
-    unsigned int X = mu_max(Source->x, Clip.x);
-    unsigned int Width = mu_min(Source->x + Source->w, Clip.x + Clip.w);
-    unsigned int Height = mu_min(Source->y + Source->h, Clip.y + Clip.h);
-
-    for (unsigned int CurrentY = Y; CurrentY < Height; CurrentY++) {
-      for (unsigned int CurrentX = X; CurrentX < Width; CurrentX++) {
-        assert(InRectangle(*Source, CurrentX, CurrentY));
-        assert(InRectangle(Clip, CurrentX, CurrentY));
-        
-        uint8_t TextureColor = GetAtlasColor(Texture, CurrentX - Source->x, CurrentY - Source->y);
-        uint32_t CurrentPixel = PAP_GetPixel(CurrentX, CurrentY);
-        
-        PAP_PutPixel(CurrentX, CurrentY, CurrentPixel | ColorToNumber(mu_color(TextureColor, TextureColor, TextureColor, 255)));
-      }
-    }
-  }
- 
   
-  TextIndex = 0;
-  OtherIndex = 0;
+  BufferIndex = 0;
 }
 
-void PushRectangle(mu_Rect Source, mu_Rect Texture, mu_Color Color, bool IsText) {
-  if (OtherIndex == BUFFER_SIZE || TextIndex == BUFFER_SIZE)
+void PushRectangle(mu_Rect Source, mu_Rect Texture, mu_Color Color) {
+  if (BufferIndex == BUFFER_SIZE)
     FlushBuffers();
-  
-  unsigned int *Index = IsText ? &TextIndex : &OtherIndex;
-  TextureData *Data = IsText ? TextData : OtherData;
-  
-  Data[*Index].Texture = Texture;
-  Data[*Index].Source = Source;
-  Data[*Index].Color = Color;
-  
-  ++*Index;
+
+  TextureBuffer[BufferIndex] = Texture;
+  SourceBuffer[BufferIndex] = Source;
+  ColorBuffer[BufferIndex] = Color;
+
+  BufferIndex += 1;
 }
 
 void r_set_clip_rect(mu_Rect Rect) {
@@ -134,7 +111,7 @@ void r_set_clip_rect(mu_Rect Rect) {
 }
 
 void r_draw_rect(mu_Rect Rect, mu_Color Color) {
-  PushRectangle(Rect, atlas[ATLAS_WHITE], Color, false);
+  PushRectangle(Rect, atlas[ATLAS_WHITE], Color);
 }
 
 void r_draw_text(const char *Text, mu_Vec2 Position, mu_Color Color) {
@@ -149,7 +126,7 @@ void r_draw_text(const char *Text, mu_Vec2 Position, mu_Color Color) {
     Destination.w = Source.w;
     Destination.h = Source.h;
     
-    PushRectangle(Destination, Source, Color, true);
+    PushRectangle(Destination, Source, Color);
 
     Destination.x += Destination.w;
   }
@@ -161,7 +138,7 @@ void r_draw_icon(int IconID, mu_Rect Rect, mu_Color Color) {
   unsigned int X = Rect.x + (Rect.w - Source.w) / 2;
   unsigned int Y = Rect.y + (Rect.h - Source.h) / 2;
 
-  PushRectangle(mu_rect(X, Y, Source.w, Source.h), Source, Color, false);
+  PushRectangle(mu_rect(X, Y, Source.w, Source.h), Source, Color);
 }
 
 int r_get_text_width(const char *Text, int Length) {
